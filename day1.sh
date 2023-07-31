13c50ad949abd7d2a571dade71965ed0d85af699
Aljozy <aljozy2@yahoo.com>
2023-08-01 00:48:38 +0530

    day1 task

---
+++ day1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#set -x
+#shell script for day1
+
+echo "First script "
+
+var1="hello"
+var2="world"
+
+echo "$var1+$var2"
+
+echo "shell path: "$SHELL
+echo "current user: "$USER
+echo "Operating system type: "$OSTYPE
+
+echo "search all text files \n"ls /home/vagrant/shellscript *.sh
+
